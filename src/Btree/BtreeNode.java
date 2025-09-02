1b9acc618cf2303fd18372ffbf877a8bbb28b196
Vedant Patil <vedant.patil0516@gmail.com>
2025-09-02 16:01:24 +0530

    b+tree-structure

--- src/Btree/BtreeNode.java
+++ src/Btree/BtreeNode.java
@@ -1,16 +1,40 @@
 public class BtreeNode {
     int[] keys;
     BtreeNode[] children;
-    int maxDegree;
+    int order;
     boolean isLeaf;
     int numKeys;
+    BtreeNode nextNode; // Pointer to the next leaf node in the linked list
+    long[] dataPointer;// Array to store pointers to actual data rows (only used in leaf nodes)
 
     public BtreeNode(int maxDegree, boolean isLeaf) {
-        this.maxDegree = maxDegree;//number of children for each node
+        this.order = maxDegree;//number of children for each node
         this.isLeaf = isLeaf;//to check whether the node is a leaf node or not
         this.keys = new int[maxDegree -1];
         this.children = new BtreeNode[maxDegree];
         this.numKeys = 0;
+        this.nextNode = null;
+        if(isLeaf)
+        {
+            this.dataPointer = new long[order - 1];
+        }
+        else {
+            this.dataPointer = null;
+        }
     }
 }
+/*
+   -> int[] keys: An array that holds the sorted key values within this node to guide searches.
 
+   ->  int order: Defines the maximum number of children a node can have, setting the tree's capacity.
+
+   ->  BtreeNode[] children: An array of pointers to the child nodes of this internal node.
+
+   ->  boolean isLeaf: A flag that is `true` if this is a bottom-level node, `false` otherwise.
+
+   ->  int numKeys: Counts the number of keys currently stored in this node.
+
+  ->   BtreeNode nextNode: A pointer that links a leaf node to the next leaf node in sequence.
+
+    -> long[] dataPointers: An array in leaf nodes that stores the disk addresses of the actual data rows.
+ */
