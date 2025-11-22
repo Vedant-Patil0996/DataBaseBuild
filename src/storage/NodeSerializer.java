ef63f7bad021b5d6b40fed29842e0294fda4d456
Vedant Patil <vedant.patil0516@gmail.com>
2025-11-22 20:01:00 +0530

    serializer-changes

--- src/storage/NodeSerializer.java
+++ src/storage/NodeSerializer.java
@@ -2,6 +2,7 @@ package storage;
 
 import java.nio.ByteBuffer;
 import Btree.*;
+
 import java.util.*;
 public class NodeSerializer {
     public static byte[] serialize(BtreeNode node) {
@@ -11,22 +12,48 @@ public class NodeSerializer {
         ByteBuffer buffer = ByteBuffer.allocate(Pager.SIZE);
         buffer.putInt(isLeaf);
         buffer.putInt(keys);
-
+        buffer.putLong(node.parentId);
+        buffer.putLong(node.nextNodeId);
         if(isLeaf==1)
         {
-            buffer.putLong(-1);
-            for (int i = 0; i < node.numKeys; i++) {
-                buffer.putLong(node.dataPointer[i]);
-                buffer.putInt(node.keys[i]);
+            for(int i=0;i<node.order;i++)
+            {
+                if (i < node.numKeys)
+                {
+                    buffer.putLong(node.dataPointer[i]);
+                    buffer.putInt(node.keys[i]);
+                }
+                else
+                {
+                    buffer.putLong(-1);
+                    buffer.putInt(0);
+                }
             }
         }
         else
         {
-            buffer.putLong(-1);
 
-            for (int i = 0; i < node.numKeys; i++) {
-                buffer.putInt(node.keys[i]);
-                buffer.putLong(-1);
+            for(int i =0;i<node.order;i++)
+            {
+                if (i<node.numKeys)
+                {
+                    buffer.putInt(node.keys[i]);
+                }
+                else
+                {
+                    buffer.putInt(0);
+                }
+            }
+            for(int i=0;i<=node.order+1;i++)
+            {
+                if (i<=node.numKeys)
+                {
+                    buffer.putLong(node.childrenId[i]);
+                }
+                else
+                {
+                    buffer.putLong(-1);
+                }
             }
         }
         return buffer.array();
@@ -37,28 +64,49 @@ public class NodeSerializer {
 
         boolean isLeaf = buffer.getInt() == 1;
         int numKeys = buffer.getInt();
-
+        long parentNodeId = buffer.getLong();
+        long nextNodeId = buffer.getLong();
         BtreeNode node = new BtreeNode(order, isLeaf );
         node.numKeys = numKeys;
-
+        node.nextNodeId = nextNodeId;
+        node.parentId = parentNodeId;
         if(isLeaf)
         {
-            long nextNodeId = buffer.getLong();
-            for (int i = 0; i < numKeys; i++) {
+            for(int i=0;i<node.order;i++)
+            {
                 node.dataPointer[i] = buffer.getLong();
                 node.keys[i] = buffer.getInt();
             }
+
         }
         else
         {
-
-            long firstChildId = buffer.getLong();
-
-            for (int i = 0; i < numKeys; i++) {
+            for (int i = 0; i < node.order; i++)
+            {
                 node.keys[i] = buffer.getInt();
-                long childId = buffer.getLong();
+            }
+            for(int i=0;i<=node.order;i++)
+            {
+                node.childrenId[i]= buffer.getLong();
             }
         }
         return node;
     }
+
+    /*
+    public static void main(String[] args) throws Exception {
+
+        BtreeNode b1 = new BtreeNode(4,false);
+        b1.keys=new int[]{1,2,3};
+        b1.childrenId = new long[]{12,19,21,22};
+        b1.parentId = 67;
+        byte[] arr1 = serialize(b1);
+        System.out.println(Arrays.toString(arr1));
+        BtreeNode b2 = deserialize(arr1,4);
+        byte[] arr2 = serialize(b2);
+        System.out.println(Arrays.toString(arr2));
+
+    }
+    */
+
 }
